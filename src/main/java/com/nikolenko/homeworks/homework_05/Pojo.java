5a70cd82d13655b0203103a296991388a0a04c7b
Vova-Nik <vmaseich@gmail.com>
2020-09-21 19:04:53 +0300

    Task of Homework 5 made (Order class)

--- src/main/java/com/nikolenko/homeworks/homework_05/Pojo.java
+++
@@ -1,5 +0,0 @@
-package com.nikolenko.homeworks.homework_05;
-
-public class Pojo {
-
-}
--- src/main/java/com/nikolenko/homeworks/homework_05/main.java
+++ src/main/java/com/nikolenko/homeworks/homework_05/main.java
@@ -1,8 +1,20 @@
 package com.nikolenko.homeworks.homework_05;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 public class main {
 
     public static void main(String[] args) {
+
+        Order orderDef = new Order();
+        System.out.println("Order default " + orderDef);
+
+        Date dt = new Date();
+        dt.setTime(dt.getTime() + 86400000);
+        Order order = new Order("Pizza", dt, 1200,120 );
+        System.out.println("Order spec " + order);
     }
 }
 
