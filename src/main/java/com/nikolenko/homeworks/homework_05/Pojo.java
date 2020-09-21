7a236cde293c1f35606be77b93fd866fd4218d0e
Vova-Nik <vmaseich@gmail.com>
2020-09-21 17:02:58 +0300

    Wery beginning. Commit just to transport from work PC to Home PC

---
+++ src/main/java/com/nikolenko/homeworks/homework_05/Pojo.java
@@ -0,0 +1,5 @@
+package com.nikolenko.homeworks.homework_05;
+
+public class Pojo {
+
+}
---
+++ src/main/java/com/nikolenko/homeworks/homework_05/main.java
@@ -0,0 +1,12 @@
+package com.nikolenko.homeworks.homework_05;
+
+public class main {
+
+    public static void main(String[] args) {
+    }
+}
+
+
+
+
+
