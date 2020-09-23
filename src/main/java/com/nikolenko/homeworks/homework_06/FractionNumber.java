04e674bda1694801f773368d9fad0b092bdf3df0
Vova-Nik <vmaseich@gmail.com>
2020-09-23 16:41:40 +0300

    Go home

--- src/main/java/com/nikolenko/homeworks/homework_06/FractionNumber.java
+++ src/main/java/com/nikolenko/homeworks/homework_06/FractionNumber.java
@@ -1,10 +1,74 @@
 package com.nikolenko.homeworks.homework_06;
 
+import java.util.Objects;
+
 public class FractionNumber {
-    int numerator;
-    int denominator;
-    public FractionNumber(){
+    private final int numerator;
+    private final int denominator;
+
+    private int inumerator;
+    private int idenominator;
+
+    public FractionNumber() {
         numerator = 0;
         denominator = 1;
+    }
+
+    public FractionNumber(int numerator, int denominator) {
+        this.numerator = numerator;
+        this.denominator = denominator;
+        normalize();
+    }
+
+    private void normalize() {
+        inumerator = numerator;
+        idenominator = denominator;
+        if (idenominator == 0) {
+            idenominator = 1;
+        }
+    }
+
+
+    public FractionNumber plus(FractionNumber fn) {
+        return this;
+    }
+
+    public FractionNumber minus(FractionNumber fn) {
+        return this;
+    }
+
+    public FractionNumber multiply(FractionNumber fn) {
+        return this;
+    }
+
+    public FractionNumber divide(FractionNumber fn) {
+        return this;
+    }
+
+
+    public String getValue() {
+        if (denominator == 0) {
+            return "Dividing by zerro error";
+        }
+        double dd = (double) numerator / denominator;
+        return String.valueOf(dd);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        FractionNumber that = (FractionNumber) o;
+        return numerator == that.numerator &&
+                denominator == that.denominator;
+    }
+
+    @Override
+    public String toString() {
+        return numerator + "/" + denominator;
+    }
+
+    public String toPrintg() {
+        return numerator + "/" + denominator + "   " + inumerator + "/" + idenominator;
     }
 }
--- src/main/java/com/nikolenko/homeworks/homework_06/Homework_06.java
+++ src/main/java/com/nikolenko/homeworks/homework_06/Homework_06.java
@@ -9,6 +9,25 @@ public class Homework_06 {
     public static void main(String[] args) {
 
         System.out.println("HW6");
+        FractionNumber fr = new FractionNumber();
+        System.out.println(fr.toString());
+        fr=null;
+        FractionNumber fr12 = new FractionNumber(1, 2);
+        FractionNumber fr_12 = new FractionNumber(1, 2);
+        FractionNumber fr13 = new FractionNumber(1, 3);
+        System.out.println("1/2 equals to 1/3 " +fr12.equals(fr13));
+        System.out.println("1/2 equals to 1/2 " +fr12.equals(fr_12));
+
+        System.out.println(fr13.getValue());
+
+        FractionNumber fr10 = new FractionNumber(1, 0);
+        System.out.println("1/0 == :) " + fr10.getValue());
+
+        FractionNumber fr24 = new FractionNumber(8, 24);
+        System.out.println(fr24.toPrintg());
+
+
+
 
 
     }
