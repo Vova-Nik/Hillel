2079f648cec552a76914435d8b8e17d2c3988456
Vova-Nik <vmaseich@gmail.com>
2020-09-23 14:45:19 +0300

    Branch hw6 with correspondent classes created once more

---
+++ src/main/java/com/nikolenko/homeworks/homework_06/FractionNumber.java
@@ -0,0 +1,10 @@
+package com.nikolenko.homeworks.homework_06;
+
+public class FractionNumber {
+    int numerator;
+    int denominator;
+    public FractionNumber(){
+        numerator = 0;
+        denominator = 1;
+    }
+}
---
+++ src/main/java/com/nikolenko/homeworks/homework_06/Homework_06.java
@@ -0,0 +1,15 @@
+package com.nikolenko.homeworks.homework_06;
+
+import com.nikolenko.homeworks.homework_05.Order;
+
+import java.util.Date;
+
+public class Homework_06 {
+
+    public static void main(String[] args) {
+
+        System.out.println("HW6");
+
+
+    }
+}
